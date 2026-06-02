79bd5ddb1003b75963c20f42f36e99696864ace6
moxxxskaifa <moxxxskaifa@users.noreply.github.com>
2026-06-02 18:36:07 +0800

    Add IPA build + App Store upload workflow

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,15 @@
 import SwiftUI
 import ComposeApp
-@main struct iOSApp: App {
+
+@main
+struct iOSApp: App {
     var body: some Scene {
-        WindowGroup { ContentView() }
+        WindowGroup {
+            ContentView()
+        }
     }
 }
+
 struct ContentView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         MainViewControllerKt.MainViewController()
